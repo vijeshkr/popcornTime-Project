f7ea7433484eec198f068c21597aa688d49f45ac
Vijesh KR <vijeshkr17@gmail.com>
2024-04-13 01:18:17 +0530

    profile page started

--- lib/pages/profile_screen.dart
+++ lib/pages/profile_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../constants/apptheme.dart';
+
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
 
@@ -10,6 +12,76 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: AppTheme.statusBar,
+        title: Text(
+          'Profile',
+          style: TextStyle(
+            color: Colors.white
+          ),
+        ),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: Container(
+        height: double.maxFinite,
+        width: double.maxFinite,
+        child: Stack(
+          children: [
+            Container(
+              height: 150,
+              width: double.maxFinite,
+              decoration: BoxDecoration(
+                color: AppTheme.statusBar,
+                borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20),
+                bottomRight: Radius.circular(20),)
+              ),
+            ),
+            Positioned(
+              top: 90,
+              left: MediaQuery.of(context).size.width * 0.5 - 60,
+              child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Center(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.white,width: 3),
+                      borderRadius: BorderRadius.circular(65),
+
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(65),
+                      child: Image.network(
+                          'https://media.licdn.com/dms/image/D4D03AQF8iQXzw-aZZg/profile-displayphoto-shrink_800_800/0/1702019275221?e=1718236800&v=beta&t=-qZHuAUC7YYsO5zy8Un0RE-befzfvMrPdxDrpoDYoZE',
+                        width: 120,
+                        height: 120,
+                      ),
+                    )
+                  ),
+                )
+              ],
+            ),),
+            Positioned(
+                top: 170,
+                left: MediaQuery.of(context).size.width * 0.53 ,
+                child: Container(
+                  height: 50,
+                  width: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(25),
+                    color: AppTheme.statusBar,
+                  ),
+                  child: Icon(
+                    Icons.camera_alt,
+                    color: Colors.white,
+                  ),
+
+            ),),
+          ],
+        ),
+      ),
+    );
   }
 }
