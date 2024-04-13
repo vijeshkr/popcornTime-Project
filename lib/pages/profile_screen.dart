41ed64a7516d7386727db1f1df4a7239bff9a65f
Vijesh KR <vijeshkr17@gmail.com>
2024-04-13 12:20:54 +0530

    profile page ongoing

--- lib/pages/profile_screen.dart
+++ lib/pages/profile_screen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 import '../constants/apptheme.dart';
 
@@ -17,55 +19,223 @@ class _ProfileScreenState extends State<ProfileScreen> {
         backgroundColor: AppTheme.statusBar,
         title: Text(
           'Profile',
-          style: TextStyle(
-            color: Colors.white
-          ),
+          style: TextStyle(color: Colors.white),
         ),
         centerTitle: true,
         elevation: 0,
       ),
-      body: Container(
-        height: double.maxFinite,
-        width: double.maxFinite,
-        child: Stack(
-          children: [
-            Container(
-              height: 150,
-              width: double.maxFinite,
-              decoration: BoxDecoration(
-                color: AppTheme.statusBar,
-                borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20),
-                bottomRight: Radius.circular(20),)
+      body: SingleChildScrollView(
+        child: Container(
+          width: double.maxFinite,
+          child: Stack(
+            children: [
+              Container(
+                height: 150,
+                width: double.maxFinite,
+                decoration: BoxDecoration(
+                    color: AppTheme.statusBar,
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(20),
+                      bottomRight: Radius.circular(20),
+                    )),
               ),
-            ),
-            Positioned(
-              top: 90,
-              left: MediaQuery.of(context).size.width * 0.5 - 60,
-              child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Center(
-                  child: Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.white,width: 3),
-                      borderRadius: BorderRadius.circular(65),
-
+              Padding(
+                padding: EdgeInsets.only(top: 90),
+                // top: 90,
+                // left: MediaQuery.of(context).size.width * 0.5 - 60,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Center(
+                      child: Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.white, width: 3),
+                            borderRadius: BorderRadius.circular(65),
+                          ),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(65),
+                            child: Image.network(
+                              'https://media.licdn.com/dms/image/D4D03AQF8iQXzw-aZZg/profile-displayphoto-shrink_800_800/0/1702019275221?e=1718236800&v=beta&t=-qZHuAUC7YYsO5zy8Un0RE-befzfvMrPdxDrpoDYoZE',
+                              width: 120,
+                              height: 120,
+                            ),
+                          )),
                     ),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(65),
-                      child: Image.network(
-                          'https://media.licdn.com/dms/image/D4D03AQF8iQXzw-aZZg/profile-displayphoto-shrink_800_800/0/1702019275221?e=1718236800&v=beta&t=-qZHuAUC7YYsO5zy8Un0RE-befzfvMrPdxDrpoDYoZE',
-                        width: 120,
-                        height: 120,
+                    SizedBox(height: 10,),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 70,),
+                      child: Text('Name'),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20,),
+                      child: TextFormField(
+                        readOnly: true,
+                        style: TextStyle(
+                          color: Colors.black
+                        ),
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                          ),
+                          prefixIcon: Padding(
+                            padding: const EdgeInsets.all(4),
+                            child: Container(
+                              height: 25,
+                              width: 25,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                color: Color(0xffe4edff),
+                              ),
+                              child: Icon(
+                                Icons.person_outline,
+                                color: Color(0xff4c7eff),
+                              ),
+                            ),
+                          ),
+                          suffixIcon: GestureDetector(
+                            onTap: (){
+                
+                            },
+                            child: Padding(
+                              padding: const EdgeInsets.all(4),
+                              child: Icon(
+                                Icons.edit_outlined,
+                                color: Color(0xff4c7eff),
+                              ),
+                            ),
+                          ),
+                        ),
                       ),
-                    )
-                  ),
-                )
-              ],
-            ),),
-            Positioned(
+                    ),
+                
+                    SizedBox(height: 10,),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 70,),
+                      child: Text('Email'),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20,),
+                      child: TextFormField(
+                        readOnly: true,
+                        style: TextStyle(
+                            color: Colors.black
+                        ),
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                          ),
+                          prefixIcon: Padding(
+                            padding: const EdgeInsets.all(4),
+                            child: Container(
+                              height: 25,
+                              width: 25,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                color: Color(0xffe8dbff),
+                              ),
+                              child: Icon(
+                                Icons.email_outlined,
+                                color: Color(0xffcc80ea),
+                              ),
+                            ),
+                          ),
+                          suffixIcon: GestureDetector(
+                            onTap: (){
+                
+                            },
+                            child: Padding(
+                              padding: const EdgeInsets.all(4),
+                              child: Icon(
+                                Icons.edit_outlined,
+                                color: Color(0xff4c7eff),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                
+                    SizedBox(height: 10,),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 70,),
+                      child: Text('Mobile'),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20,),
+                      child: TextFormField(
+                        readOnly: true,
+                        style: TextStyle(
+                            color: Colors.black
+                        ),
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                          ),
+                          prefixIcon: Padding(
+                            padding: const EdgeInsets.all(4),
+                            child: Container(
+                              height: 25,
+                              width: 25,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                color: Color(0xffcceff0),
+                              ),
+                              child: Icon(
+                                Icons.phone_android_outlined,
+                                color: Color(0xff48eb9f),
+                              ),
+                            ),
+                          ),
+                          suffixIcon: GestureDetector(
+                            onTap: (){
+                
+                            },
+                            child: Padding(
+                              padding: const EdgeInsets.all(4),
+                              child: Icon(
+                                Icons.edit_outlined,
+                                color: Color(0xff4c7eff),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Divider(thickness: 1,color: Colors.grey,),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 30),
+                      child: Text('DOCUMENTATION'),
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.settings_outlined,
+                      size: 16,),
+                      title: Text('Settings'),
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.privacy_tip_outlined,
+                        size: 16,),
+                      title: Text('Privacy Policy'),
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.share_outlined,
+                        size: 16,),
+                      title: Text('Share'),
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.exit_to_app_outlined,
+                        color: Colors.red,
+                        size: 16,),
+                      title: Text('Logout',
+                      style: TextStyle(
+                        color: Colors.red,
+                      ),),
+                    ),
+                  ],
+                ),
+              ),
+              Positioned(
                 top: 170,
-                left: MediaQuery.of(context).size.width * 0.53 ,
+                left: MediaQuery.of(context).size.width * 0.53,
                 child: Container(
                   height: 50,
                   width: 50,
@@ -77,9 +247,10 @@ class _ProfileScreenState extends State<ProfileScreen> {
                     Icons.camera_alt,
                     color: Colors.white,
                   ),
-
-            ),),
-          ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
